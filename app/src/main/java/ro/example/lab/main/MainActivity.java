402acb1a522f773e6b6b040456c6c6860fe08de3
Cosmin Stefan <cosmin@greenerpastures.dk>
2019-05-15 19:41:04 +0300

    Added main app structure

---
+++ app/src/main/java/ro/example/lab/main/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.example.lab.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ro.example.lab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
