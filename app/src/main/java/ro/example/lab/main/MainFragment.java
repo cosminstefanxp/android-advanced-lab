be96718ba68e0dff36a44dd57ea716c0729d5cb2
Eugen Cutic <eugen_cutic@yahoo.com>
2019-05-15 20:51:31 +0300

    created main fragment \w bottom navigation and nav graph; \wo navigation listener and nav hst

---
+++ app/src/main/java/ro/example/lab/main/MainFragment.java
@@ -0,0 +1,33 @@
+package ro.example.lab.main;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.fragment.NavHostFragment;
+import ro.example.lab.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends NavHostFragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
