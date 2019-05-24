b67ad4c8ed9883c27ae51a2c8f5d22052a416c3b
Ana632 <anapintilie3@yahoo.com>
2019-05-24 19:22:25 +0300

    Get list of stations from Firestore Database

--- app/src/main/java/ro/example/lab/map/MapFragment.java
+++ app/src/main/java/ro/example/lab/map/MapFragment.java
@@ -2,20 +2,29 @@ package ro.example.lab.map;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import ro.example.lab.R;
+import ro.example.lab.data.Station;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFragment extends Fragment {
 
+    private TextView mTextViewStations;
+
+
+    private MapViewModel viewModel;
 
     public MapFragment() {
         // Required empty public constructor
@@ -23,10 +32,30 @@ public class MapFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mTextViewStations = view.findViewById(R.id.textView_stations);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        configureViewModel();
+    }
+
+    private void configureViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MapViewModel.class);
+        viewModel.init("RO");
+        viewModel.getCountryStations().observe(this, this::updateUIWithData);
+    }
+
+    private void updateUIWithData(List<Station> stations) {
+        if (stations != null) {
+            mTextViewStations.setText(String.valueOf(stations.size()));
+        }
     }
 
 }
